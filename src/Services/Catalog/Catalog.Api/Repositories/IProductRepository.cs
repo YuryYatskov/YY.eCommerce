5662cecc8eec11339138d3c4a40852295d6de611
Yury Yatskov <Y.Yatskov@hotmail.com>
2022-04-06 12:56:01 +0300

    Code comments.

--- src/Services/Catalog/Catalog.Api/Repositories/IProductRepository.cs
+++ src/Services/Catalog/Catalog.Api/Repositories/IProductRepository.cs
@@ -2,20 +2,56 @@
 
 namespace Catalog.Api.Repositories
 {
+    /// <summary>
+    /// Product repository.
+    /// </summary>
     public interface IProductRepository
     {
+        /// <summary>
+        /// Get all products.
+        /// </summary>
+        /// <returns> All products. </returns>
         Task<IEnumerable<Product>> GetProductsAsync();
 
+        /// <summary>
+        /// Get a product by identifier.
+        /// </summary>
+        /// <param name="id"> Identifier of product.  </param>
+        /// <returns> A product. </returns>
         Task<Product> GetProductAsync(string id);
 
+        /// <summary>
+        /// Get products by name.
+        /// </summary>
+        /// <param name="name"> A name. </param>
+        /// <returns> Products. </returns>
         Task<IEnumerable<Product>> GetProductByNameAsync(string name);
 
+        /// <summary>
+        /// Get products by category.
+        /// </summary>
+        /// <param name="categoryName"> A category. </param>
+        /// <returns> Products. </returns>
         Task<IEnumerable<Product>> GetProductByCategoryAsync(string categoryName);
 
+        /// <summary>
+        /// Create a product.
+        /// </summary>
+        /// <param name="product"> A product. </param>
         Task CreateProductAsync(Product product);
 
+        /// <summary>
+        /// Change a product.
+        /// </summary>
+        /// <param name="product"> A product. </param>
+        /// <returns> Change status. </returns>
         Task<bool> UpdateProductAsync(Product product);
 
+        /// <summary>
+        /// Delete a product.
+        /// </summary>
+        /// <param name="id"> Identifier of product.  </param>
+        /// <returns> Delete status. </returns>
         Task<bool> DeleteProductAsync(string id);
     }
 }
