167090aee6d14a6cd68f0c6d1b3207bfcfbb9077
Yury Yatskov <Y.Yatskov@gmail.com>
2022-06-09 19:52:59 +0300

    Changed the ocelot local config.

--- src/ApiGateways/OcelotApiGw/Program.cs
+++ src/ApiGateways/OcelotApiGw/Program.cs
@@ -4,7 +4,7 @@ using Ocelot.Middleware;
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Host.ConfigureAppConfiguration((hostingContext, config) => {
-    config.AddJsonFile($"ocelot.{hostingContext.HostingEnvironment}.json", true, true);
+    config.AddJsonFile($"ocelot.{hostingContext.HostingEnvironment.EnvironmentName}.json", true, true);
 });
 
 builder.Host.ConfigureLogging((hostingContext, loggingBuilder) =>
@@ -18,7 +18,7 @@ builder.Services.AddOcelot();
 
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+//app.MapGet("/", () => "Hello World!");
 
 await app.UseOcelot();
 
